cec267bebff80f5a5f32b76c806490902ead7448
Diego Keller <keller.diego@gmail.com>
2023-02-28 20:14:19 -0300

    Tarefas 3

--- tarefas3/index.js
+++ tarefas3/index.js
@@ -1,7 +1,52 @@
 console.log('Ola Mundo!');
 
 const tarefaService = require('./tarefa.service');
+const express = require("express");
+const cors = require('cors')
 
-(async function() {
-    await tarefaService.buscarTarefa(3);
-})();
+const app = express();
+
+app.use(express.json());
+
+const corsOptions = {
+    origin: 'http://127.0.0.1:5500'
+}
+app.use(cors(corsOptions))
+
+// Nos decidimos o verbo
+// Nos decidimos a rota
+app.get("/ola", (req, res) => {
+    // nos decidimos o retorno
+    res.json({ message: "ola :-D" });
+});
+
+app.post("/somar", (req, res) => {
+    const soma = req.body.numero1 + req.body.numero2;
+    res.json({resultado: soma});
+})
+
+app.get("/tarefas", async (req, res) => {
+    res.json(await tarefaService.buscarTarefas());
+})
+
+app.get("/tarefas/total", async (req, res) => {
+    res.json(await tarefaService.buscarTotalTarefas());
+})
+
+app.post("/tarefas", async (req, res) => {
+    res.json(await tarefaService.incluirTarefa(req.body.titulo));
+})
+
+app.patch("/tarefas/:id/concluir", async (req, res) => {
+    res.json(await tarefaService.concluirTarefa(req.params.id));
+})
+
+app.delete("/tarefas/:id", async (req, res) => {
+    res.json(await tarefaService.excluirTarefa(req.params.id));
+})
+
+// Iniciar o servidor na porta
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
--- tarefas3/tarefa.service.js
+++ tarefas3/tarefa.service.js
@@ -7,26 +7,52 @@ async function buscarTarefas() {
     const connection = await mysql.createConnection(config.db);
     const result = await connection.execute('SELECT * FROM tarefas');
     connection.end();
-    console.log(result[0]);
+    return result[0];
+}
+
+async function buscarTotalTarefas() {
+    const connection = await mysql.createConnection(config.db);
+    const result = await connection.execute('SELECT COUNT(*) as total FROM tarefas');
+    connection.end();
+    return result[0];
 }
 
 async function buscarTarefa(id) {
     const connection = await mysql.createConnection(config.db);
     const result = await connection.execute('SELECT * FROM tarefas WHERE id = ' + id);
     connection.end();
-    console.log(result[0]);
+    return result[0];
 }
 
 async function incluirTarefa(titulo) {
+    const connection = await mysql.createConnection(config.db);
+    const result = await connection.execute(
+        `INSERT INTO tarefas (titulo, criada_em, concluida) VALUES ('${titulo}', NOW(), 0)`);
+    connection.end();
+    return result[0];
 }
 
 async function excluirTarefa(id) {
+    const connection = await mysql.createConnection(config.db);
+    const result = await connection.execute(
+        'DELETE FROM tarefas WHERE id = ' + id);
+    connection.end();
+    return result[0];
 }
 
 async function concluirTarefa(id) {
+    const connection = await mysql.createConnection(config.db);
+    const result = await connection.execute(
+        'UPDATE tarefas SET concluida = 1 WHERE id = ' + id);
+    connection.end();
+    return result[0];
 }
 
 module.exports = {
     buscarTarefas,
-    buscarTarefa
+    buscarTarefa,
+    concluirTarefa,
+    excluirTarefa,
+    incluirTarefa,
+    buscarTotalTarefas
 }
